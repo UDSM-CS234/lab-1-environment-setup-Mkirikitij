7f5dd3903ae251a445d19285b09dec2dab9c4d3c
Mkirikitij <86766324+Mkirikitij@users.noreply.github.com>
2022-05-24 14:54:35 +0300

    Add files via upload

---
+++ Displacement.java
@@ -0,0 +1,10 @@
+public class Displacement {
+    public static void main (String[] args){
+        double a = -9.81;
+        int t=10;
+        double v1=0;
+        double x1=0;
+        double x2=(0.5*a*t*t)+v1*t+x1;
+        System.out.println(x2);
+    }
+}
